1541e535ca3db5b840e9832e7952fee01e37a8ae
zox <153457825+zoxile@users.noreply.github.com>
2026-01-28 10:42:54 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -18,21 +18,6 @@ RegisterCommand('admin', function()
             action = 'openAdmin',
             data = data.serverData
         })
-
-        CreateThread(function()
-            while isOpen do
-                Wait(Config.PlayerUpdateInterval)
-
-                ESX.TriggerServerCallback('esx-adminmenu:server:getInitData', function(update)
-                    if update then
-                        SendNUIMessage({
-                            action = 'updatePlayers',
-                            data = update.serverData
-                        })
-                    end
-                end)
-            end
-        end)
     end)
 end)
 
